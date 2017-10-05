5d457c2461e82f7279eb34910db8373b1e088604
select766 <select766@outlook.jp>
2017-10-06 08:43:01 +0900

    cythonの静的ビルド

--- neneshogi/position.py
+++ neneshogi/position.py
@@ -5,9 +5,6 @@
 from typing import List, Tuple
 
 import numpy as np
-import pyximport
-
-pyximport.install(setup_args={'include_dirs': np.get_include()})
 from . import position_acc
 
 
--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-from setuptools import setup, find_packages
+# Compile cython module:
+# python setup.py build_ext --inplace
+# Add to import path:
+# python setup.py develop
+
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize, build_ext
+import numpy as np
+
+extensions = [
+    Extension("*", ["neneshogi/*.pyx"],
+              include_dirs=[np.get_include()])
+]
 
 setup(
     name="neneshogi",
     version="0.1",
     packages=find_packages(),
-    test_suite='test'
+    test_suite='test',
+    cmdclass={"build_ext": build_ext},
+    ext_modules=cythonize(extensions)
 )
