631dab23aa2081791fc5028e9f8bb64513156707
select766 <select766@outlook.jp>
2017-10-08 18:39:39 +0900

    _generate_move_move のcython化

---
+++ neneshogi/move.py
@@ -0,0 +1,278 @@
+from typing import List, Tuple
+
+import numpy as np
+
+
+class Color:
+    """
+    手番定数
+    """
+    BLACK = 0  # 先手
+    WHITE = 1  # 後手
+    COLOR_NB = 2
+
+    @staticmethod
+    def invert(color: int) -> int:
+        """
+        手番の反転
+        :param color:
+        :return:
+        """
+        return 1 - color
+
+
+class Piece:
+    """
+    駒定数
+    """
+
+    # 駒種
+    NO_PIECE = 0
+    PAWN = 1  # 歩
+    LANCE = 2  # 香
+    KNIGHT = 3  # 桂
+    SILVER = 4  # 銀
+    BISHOP = 5  # 角
+    ROOK = 6  # 飛
+    GOLD = 7  # 金
+    KING = 8  # 玉
+    PRO_PAWN = 9  # と
+    PRO_LANCE = 10  # 成香
+    PRO_KNIGHT = 11  # 成桂
+    PRO_SILVER = 12  # 成銀
+    HORSE = 13  # 馬
+    DRAGON = 14  # 竜
+    QUEEN = 15  # 未使用
+
+    # 先手の駒
+    B_PAWN = 1
+    B_LANCE = 2
+    B_KNIGHT = 3
+    B_SILVER = 4
+    B_BISHOP = 5
+    B_ROOK = 6
+    B_GOLD = 7
+    B_KING = 8
+    B_PRO_PAWN = 9
+    B_PRO_LANCE = 10
+    B_PRO_KNIGHT = 11
+    B_PRO_SILVER = 12
+    B_HORSE = 13
+    B_DRAGON = 14
+    B_QUEEN = 15  # 未使用
+
+    # 後手の駒
+    W_PAWN = 17
+    W_LANCE = 18
+    W_KNIGHT = 19
+    W_SILVER = 20
+    W_BISHOP = 21
+    W_ROOK = 22
+    W_GOLD = 23
+    W_KING = 24
+    W_PRO_PAWN = 25
+    W_PRO_LANCE = 26
+    W_PRO_KNIGHT = 27
+    W_PRO_SILVER = 28
+    W_HORSE = 29
+    W_DRAGON = 30
+    W_QUEEN = 31  # 未使用
+
+    PIECE_NB = 32
+    PIECE_ZERO = 0
+    PIECE_PROMOTE = 8
+    PIECE_WHITE = 16
+    PIECE_RAW_NB = 8
+    PIECE_HAND_ZERO = PAWN  # 手駒の駒種最小値
+    PIECE_HAND_NB = KING  # 手駒の駒種最大値+1
+
+    PIECE_FROM_CHAR_TABLE = {"P": B_PAWN, "L": B_LANCE, "N": B_KNIGHT, "S": B_SILVER,
+                             "B": B_BISHOP, "R": B_ROOK, "G": B_GOLD, "K": B_KING,
+                             "+P": B_PRO_PAWN, "+L": B_PRO_LANCE, "+N": B_PRO_KNIGHT, "+S": B_PRO_SILVER,
+                             "+B": B_HORSE, "+R": B_DRAGON,
+                             "p": W_PAWN, "l": W_LANCE, "n": W_KNIGHT, "s": W_SILVER,
+                             "b": W_BISHOP, "r": W_ROOK, "g": W_GOLD, "k": W_KING,
+                             "+p": W_PRO_PAWN, "+l": W_PRO_LANCE, "+n": W_PRO_KNIGHT, "+s": W_PRO_SILVER,
+                             "+b": W_HORSE, "+r": W_DRAGON, }
+    CHAR_FROM_PIECE_TABLE = {v: k for k, v in PIECE_FROM_CHAR_TABLE.items()}
+
+    @staticmethod
+    def piece_from_char(c: str) -> int:
+        """
+        駒の文字から定数を計算、先手が大文字、後手が小文字
+        成り駒も考慮
+        :param c:
+        :return:
+        """
+        return Piece.PIECE_FROM_CHAR_TABLE[c]
+
+    @staticmethod
+    def char_from_piece(piece: int) -> str:
+        """
+        駒に対応する文字を返す、入力は駒種でも可(大文字が返る)
+        成り駒も考慮し、"+"が先頭に付加される
+        :param piece:
+        :return:
+        """
+        return Piece.CHAR_FROM_PIECE_TABLE[piece]
+
+    @staticmethod
+    def raw_pt_from_piece(piece: int) -> int:
+        """
+        駒から、成る前の駒種を計算
+        :param piece:
+        :return:
+        """
+        raw_pt = piece % Piece.PIECE_RAW_NB
+        if raw_pt == 0:
+            raw_pt = Piece.KING
+        return raw_pt
+
+    @staticmethod
+    def is_exist(piece: int) -> bool:
+        """
+        駒が存在するかどうか(空のマスでないか)を判定する
+        :param piece:
+        :return:
+        """
+        return piece > Piece.PIECE_ZERO
+
+    @staticmethod
+    def is_color(piece: int, color: int) -> bool:
+        """
+        駒が特定の色かどうか判定する
+        :param piece:
+        :param color:
+        :return:
+        """
+        if piece == Piece.PIECE_ZERO:
+            return False
+        return piece // Piece.PIECE_WHITE == color
+
+
+class Square:
+    """
+    マス定数
+    筋*9+段
+    1筋を0、1段を0に割り当てる
+    """
+    SQ_NB = 81
+
+    @staticmethod
+    def from_file_rank(file: int, rank: int) -> int:
+        return file * 9 + rank
+
+    @staticmethod
+    def from_file_rank_if_valid(file: int, rank: int) -> Tuple[int, bool]:
+        sq = file * 9 + rank
+        valid = file >= 0 and file < 9 and rank >= 0 and rank < 9
+        return sq, valid
+
+    @staticmethod
+    def file_of(sq: int) -> int:
+        """
+        筋を返す
+        :param sq:
+        :return:
+        """
+        return sq // 9
+
+    @staticmethod
+    def rank_of(sq: int) -> int:
+        """
+        段を返す
+        :param sq:
+        :return:
+        """
+        return sq % 9
+
+
+class Move:
+    """
+    指し手を表すクラス(数値定数ではない)
+    immutableとして扱う
+    """
+    move_from: int
+    move_to: int
+    move_dropped_piece: int  # 打った駒種(手番関係なし)
+    is_promote: bool
+    is_drop: bool
+
+    def __init__(self, move_from: int, move_to: int, move_dropped_piece: int, is_promote: bool, is_drop: bool):
+        self.move_from = move_from
+        self.move_to = move_to
+        self.move_dropped_piece = move_dropped_piece
+        self.is_promote = is_promote
+        self.is_drop = is_drop
+
+    @staticmethod
+    def make_move(move_from: int, move_to: int, is_promote: bool = False) -> "Move":
+        return Move(move_from, move_to, 0, is_promote, False)
+
+    @staticmethod
+    def make_move_drop(move_dropped_piece: int, move_to: int) -> "Move":
+        return Move(0, move_to, move_dropped_piece, False, True)
+
+    @staticmethod
+    def from_usi_string(move_usi: str) -> "Move":
+        """
+        USI形式の指し手からインスタンスを生成
+        :param move_usi: 例: "7g7f"
+        :return:
+        """
+        to_file = ord(move_usi[2]) - ord("1")
+        to_rank = ord(move_usi[3]) - ord("a")
+        from_file = ord(move_usi[0]) - ord("1")
+        if from_file > 8:
+            # 駒打ち("G*5b")
+            drop_pt = Piece.piece_from_char(move_usi[0])
+            return Move.make_move_drop(drop_pt, Square.from_file_rank(to_file, to_rank))
+        else:
+            from_rank = ord(move_usi[1]) - ord("a")
+            is_promote = len(move_usi) >= 5 and move_usi[4] == "+"
+            return Move.make_move(Square.from_file_rank(from_file, from_rank),
+                                  Square.from_file_rank(to_file, to_rank),
+                                  is_promote)
+
+    def to_usi_string(self) -> str:
+        """
+        USI形式の指し手文字列を作成
+        :return:
+        """
+        to_file_c = chr(Square.file_of(self.move_to) + ord("1"))
+        to_rank_c = chr(Square.rank_of(self.move_to) + ord("a"))
+        if self.is_drop:
+            drop_pt_c = Piece.char_from_piece(self.move_dropped_piece)
+            return drop_pt_c + "*" + to_file_c + to_rank_c
+        else:
+            from_file_c = chr(Square.file_of(self.move_from) + ord("1"))
+            from_rank_c = chr(Square.rank_of(self.move_from) + ord("a"))
+            if self.is_promote:
+                return from_file_c + from_rank_c + to_file_c + to_rank_c + "+"
+            else:
+                return from_file_c + from_rank_c + to_file_c + to_rank_c
+
+    def __str__(self):
+        return self.to_usi_string()
+
+    def __eq__(self, other: "Move") -> bool:
+        return self.move_from == other.move_from and self.move_to == other.move_to and \
+               self.move_dropped_piece == self.move_dropped_piece and self.is_promote == other.is_promote and \
+               self.is_drop == other.is_drop
+
+    def __hash__(self) -> int:
+        """
+        やねうら王のMoveと同じ16bit以下の数値
+        :return:
+        """
+        return self.move_to + self.move_from * 128 + self.move_dropped_piece * 128 + \
+               int(self.is_drop) * 16384 + int(self.is_promote) * 32768
+
+
+class UndoMoveInfo:
+    board: np.ndarray
+    hand: np.ndarray
+
+    def __init__(self, pos: "Position"):
+        self.board = pos.board.copy()
+        self.hand = pos.hand.copy()
--- neneshogi/position_acc.pyx
+++ neneshogi/position_acc.pyx
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 import numpy as np
+from .move import Move
 cimport cython
 cimport numpy as np
 
@@ -84,6 +85,17 @@ cdef int piece_is_exist(int piece):
     :return:
     """
     return piece > Piece.PIECE_ZERO
+
+cdef int piece_is_color(int piece, int color):
+    """
+    駒が特定の色かどうか判定する
+    :param piece:
+    :param color:
+    :return:
+    """
+    if piece == Piece.PIECE_ZERO:
+        return False
+    return piece // Piece.PIECE_WHITE == color
 
 """
 マス定数
@@ -237,3 +249,141 @@ def _in_check_black(np.ndarray[DTYPE_t, ndim=1] board) -> bool:
             return True
 
     return False
+
+cdef struct Vec:
+    int x
+    int y
+
+_SHORT_ATTACK_TABLE_py = [[],
+                       [(0, -1)],  # 歩
+                       [],  # 香
+                       [(-1, -2), (1, -2)],  # 桂
+                       [(-1, -1), (0, -1), (1, -1), (-1, 1), (1, 1)],  # 銀
+                       [],  # 角
+                       [],  # 飛
+                       [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (0, 1)],  # 金
+                       [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)],  # 玉
+                       [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (0, 1)],  # と
+                       [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (0, 1)],  # 成香
+                       [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (0, 1)],  # 成桂
+                       [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (0, 1)],  # 成銀
+                       [(0, -1), (-1, 0), (1, 0), (0, 1)],  # 馬
+                       [(-1, -1), (1, -1), (-1, 1), (1, 1)],  # 竜
+                       ]
+cdef int _SHORT_ATTACK_TABLE[15][8][2]
+cdef int _SHORT_ATTACK_TABLE_LEN[15]
+for i, d in enumerate(_SHORT_ATTACK_TABLE_py):
+    _SHORT_ATTACK_TABLE_LEN[i] = len(d)
+    for j, (x, y) in enumerate(d):
+        _SHORT_ATTACK_TABLE[i][j] = (x, y)
+
+cdef int _MAX_NON_PROMOTE_RANK_TABLE[15]
+_MAX_NON_PROMOTE_RANK_TABLE[:] = [0,
+                               3,  # 歩(必ず成る)
+                               2,  # 香(2段目では必ず成る)
+                               2,  # 桂
+                               0,  # 銀
+                               3,  # 角(必ず成る)
+                               3,  # 飛(必ず成る)
+                               0,  # 金
+                               0,
+                               0,
+                               0,
+                               0,
+                               0,
+                               0,
+                               0,
+                               ]
+
+_LONG_ATTACK_TABLE_py = [[],
+                      [],  # 歩
+                      [(0, -1)],  # 香
+                      [],  # 桂
+                      [],  # 銀
+                      [(-1, -1), (1, -1), (-1, 1), (1, 1)],  # 角
+                      [(0, -1), (-1, 0), (1, 0), (0, 1)],  # 飛
+                      [],  # 金
+                      [],  # 玉
+                      [],  # と
+                      [],  # 成香
+                      [],  # 成桂
+                      [],  # 成銀
+                      [(-1, -1), (1, -1), (-1, 1), (1, 1)],  # 馬
+                      [(0, -1), (-1, 0), (1, 0), (0, 1)],  # 竜
+                      ]
+
+cdef int _LONG_ATTACK_TABLE[15][4][2]
+cdef int _LONG_ATTACK_TABLE_LEN[15]
+for i, d in enumerate(_LONG_ATTACK_TABLE_py):
+    _LONG_ATTACK_TABLE_LEN[i] = len(d)
+    for j, (x, y) in enumerate(d):
+        _LONG_ATTACK_TABLE[i][j] = (x, y)
+
+_MAX_DROP_RANK_TABLE = [0, 1, 1, 2, 0, 0, 0, 0]
+
+@cython.boundscheck(False)
+def _generate_move_move(np.ndarray[DTYPE_t, ndim=1] board) -> List[Move]:
+    """
+    盤上の駒を動かす手をすべて生成する。
+    先手番を前提とする。
+    ただし、香車の2段目・歩・角・飛の不成りおよび行き場のない駒を生じる手は除く。
+    :return:
+    """
+    possible_moves = []
+    cdef int from_file, from_rank, from_sq, from_piece, can_promote, to_file, to_rank, to_sq, to_piece
+    cdef int x, y
+    for from_file in range(9):
+        for from_rank in range(9):
+            from_sq = square_from_file_rank(from_file, from_rank)
+            from_piece = board[from_sq]
+            if not piece_is_color(from_piece, Color.BLACK):
+                continue
+            can_promote = from_piece <= Piece.B_ROOK
+            short_attacks = _SHORT_ATTACK_TABLE[from_piece]  # type: List[Tuple[int, int]]
+            max_non_promote_rank = _MAX_NON_PROMOTE_RANK_TABLE[from_piece]  # type: int
+            # 短い利きの処理
+            for short_attack_i in range(_SHORT_ATTACK_TABLE_LEN[from_piece]):
+                x = _SHORT_ATTACK_TABLE[from_piece][short_attack_i][0]
+                y = _SHORT_ATTACK_TABLE[from_piece][short_attack_i][1]
+                to_file = from_file + x
+                to_rank = from_rank + y
+                # 盤内確認
+                to_sq = square_from_file_rank_if_valid(to_file, to_rank)
+                if to_sq < 0:
+                    continue
+                to_piece = board[to_sq]
+                # 自分の駒があるところには進めない
+                if not piece_is_color(to_piece, Color.BLACK):
+                    if to_rank >= max_non_promote_rank:
+                        # 行き場のない駒にはならない(&無意味な不成ではない)
+                        possible_moves.append(Move.make_move(from_sq, to_sq, False))
+                    if can_promote and (from_rank < 3 or to_rank < 3):
+                        # 成れる駒で、成る条件を満たす
+                        possible_moves.append(Move.make_move(from_sq, to_sq, True))
+            # 長い利きの処理
+            for long_attack_i in range(_LONG_ATTACK_TABLE_LEN[from_piece]):
+                x = _LONG_ATTACK_TABLE[from_piece][long_attack_i][0]
+                y = _LONG_ATTACK_TABLE[from_piece][long_attack_i][1]
+                to_file = from_file
+                to_rank = from_rank
+                while True:
+                    to_file += x
+                    to_rank += y
+                    to_sq = square_from_file_rank_if_valid(to_file, to_rank)
+                    if to_sq < 0:
+                        break
+                    to_piece = board[to_sq]
+                    # 自分の駒があるところには進めない
+                    if piece_is_color(to_piece, Color.BLACK):
+                        break
+                    if to_rank >= max_non_promote_rank and from_rank >= max_non_promote_rank:
+                        # 成って損がないのに成らない状況以外(角・飛)
+                        possible_moves.append(Move.make_move(from_sq, to_sq, False))
+                    if can_promote and (from_rank < 3 or to_rank < 3):
+                        # 成れる駒で、成る条件を満たす
+                        possible_moves.append(Move.make_move(from_sq, to_sq, True))
+                    if piece_is_exist(to_piece):
+                        # 白駒があるので、これ以上進めない
+                        break
+
+    return possible_moves
