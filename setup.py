5012be094d52878c7f155ef9267664c835c0ecbf
select766 <select766@outlook.jp>
2017-12-02 10:38:01 +0900

    cython対象ファイルがないエラーに対処

--- setup.py
+++ setup.py
@@ -4,19 +4,12 @@
 # python setup.py develop
 
 from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize, build_ext
 import numpy as np
 
-extensions = [
-    Extension("*", ["neneshogi/*.pyx"],
-              include_dirs=[np.get_include()])
-]
 
 setup(
     name="neneshogi",
     version="1.0",
     packages=find_packages(),
-    test_suite='test',
-    cmdclass={"build_ext": build_ext},
-    ext_modules=cythonize(extensions, annotate=True)
+    test_suite='test'
 )
