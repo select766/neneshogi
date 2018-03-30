86e7a39d6bf2cbe68beb1632ac2ea9131e1338e3
select766 <select766@outlook.jp>
2018-03-30 21:38:44 +0900

    generate training graph in val_interval

--- neneshogi/train_config.py
+++ neneshogi/train_config.py
@@ -107,17 +107,17 @@ def load_trainer(path, gpu: int = -1) -> training.Trainer:
     if extensions.PlotReport.available():
         trainer.extend(
             extensions.PlotReport(['main/loss', 'validation/main/loss'],
-                                  'epoch', file_name='loss.png'))
+                                  'iteration', val_interval, file_name='loss.png'))
         trainer.extend(
             extensions.PlotReport(['main/loss_move', 'validation/main/loss_move'],
-                                  'epoch', file_name='loss_move.png'))
+                                  'iteration', val_interval, file_name='loss_move.png'))
         trainer.extend(
             extensions.PlotReport(['main/loss_value', 'validation/main/loss_value'],
-                                  'epoch', file_name='loss_value.png'))
+                                  'iteration', val_interval, file_name='loss_value.png'))
         trainer.extend(
             extensions.PlotReport(
                 ['main/accuracy', 'validation/main/accuracy'],
-                'epoch', file_name='accuracy.png'))
+                'iteration', val_interval, file_name='accuracy.png'))
 
     trainer.extend(extensions.ProgressBar(update_interval=10))
 
